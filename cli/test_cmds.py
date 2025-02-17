e1705b931e67a856d284409a2246c0c78989b65b
Jérôme Peyras <jerome.peyras@protonmail.com>
2025-02-17 18:14:32 +0100

    add simple tests for list commands

---
+++ cli/test_cmds.py
@@ -0,0 +1,29 @@
+from typer.testing import CliRunner
+
+from .main import app
+
+runner = CliRunner()
+
+
+def test_user_list():
+    result = runner.invoke(app, ["users", "list"], input="testadmin@test.com\ntest")
+    assert result.exit_code == 0
+    assert "Users" in result.stdout
+
+
+def test_clients_list():
+    result = runner.invoke(app, ["clients", "list"], input="testadmin@test.com\ntest")
+    assert result.exit_code == 0
+    assert "Clients" in result.stdout
+
+
+def test_contracts_list():
+    result = runner.invoke(app, ["contracts", "list"], input="testadmin@test.com\ntest")
+    assert result.exit_code == 0
+    assert "Contracts" in result.stdout
+
+
+def test_events_list():
+    result = runner.invoke(app, ["events", "list"], input="testadmin@test.com\ntest")
+    assert result.exit_code == 0
+    assert "Events" in result.stdout
