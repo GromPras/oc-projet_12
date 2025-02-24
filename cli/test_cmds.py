6b47e0e980bb16df6703f076453bb425f9a3e3a9
Jérôme Peyras <jerome.peyras@protonmail.com>
2025-02-24 09:30:53 +0100

    test creating and destroying users

--- cli/test_cmds.py
+++ cli/test_cmds.py
@@ -11,6 +11,34 @@ def test_user_list():
     assert "Users" in result.stdout
 
 
+def test_user_create():
+    result = runner.invoke(
+        app,
+        [
+            "users",
+            "create",
+            "-n",
+            "'Test Create From Command'",
+            "-e",
+            "testcmd@test.com",
+            "-ph",
+            "0123456789",
+            "-d",
+            "admin",
+            "-p",
+            "test",
+        ],
+    )
+    assert result.exit_code == 0
+    assert "User created" in result.stdout
+
+
+# def test_user_delete():
+#     result = runner.invoke(app, ["users", "delete", "55"], input="y")
+#     assert result.exit_code == 0
+#     assert "User removed" in result.stdout
+
+
 def test_clients_list():
     result = runner.invoke(app, ["clients", "list"], input="testadmin@test.com\ntest")
     assert result.exit_code == 0
@@ -25,12 +53,19 @@ def test_contracts_list():
 
 def test_contracts_list_with_filters():
     result = runner.invoke(
-        app, ["contracts", "list" "--pending"], input="testadmin@test.com\ntest"
+        app, ["contracts", "list", "--pending"], input="testadmin@test.com\ntest"
     )
     assert result.exit_code == 0
     assert "Contracts" in result.stdout
     result = runner.invoke(
-        app, ["contracts", "list" "--owing"], input="testadmin@test.com\ntest"
+        app, ["contracts", "list", "--owing"], input="testadmin@test.com\ntest"
+    )
+    assert result.exit_code == 0
+    assert "Contracts" in result.stdout
+    result = runner.invoke(
+        app,
+        ["contracts", "list", "--pending", "--owing"],
+        input="testadmin@test.com\ntest",
     )
     assert result.exit_code == 0
     assert "Contracts" in result.stdout
@@ -38,5 +73,22 @@ def test_contracts_list_with_filters():
 
 def test_events_list():
     result = runner.invoke(app, ["events", "list"], input="testadmin@test.com\ntest")
+    assert result.exit_code == 0
+    assert "Events" in result.stdout
+
+
+def test_events_list_with_filters():
+    result = runner.invoke(
+        app,
+        ["events", "list", "--filter", "no-support"],
+        input="testadmin@test.com\ntest",
+    )
+    assert result.exit_code == 0
+    assert "Events" in result.stdout
+    result = runner.invoke(
+        app,
+        ["events", "list", "--filter", "assigned"],
+        input="testsupport@test.com\ntest",
+    )
     assert result.exit_code == 0
     assert "Events" in result.stdout
