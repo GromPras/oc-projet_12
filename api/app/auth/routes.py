3ad463a403edb426b9c004b9cb9d1b2c6607c618
Jérôme Peyras <jerome.peyras@protonmail.com>
2025-03-02 17:30:59 +0100

    Adds an endpoint that returns role based authorizations

--- api/app/auth/routes.py
+++ api/app/auth/routes.py
@@ -1,3 +1,4 @@
+from flask import request
 from app import db
 from app.auth import bp
 from app.auth.auth import basic_auth
@@ -12,7 +13,62 @@ def get_token():
     return {"token": token}
 
 
-@bp.route("/tokens", methods=["Get"])
+@bp.route("/tokens", methods=["GET"])
 @token_auth.login_required()
 def authenticate_token():
     return {"message": "Authenticated"}, 200
+
+
+@bp.route("/authorizations", methods=["POST"])
+@token_auth.login_required()
+def check_authorizations():
+    target = request.get_json()["target"]
+    if not target:
+        return {"message": "Bad request"}, 400
+    user_role = token_auth.current_user().role.value
+    authorizations = {
+        "admin": [
+            "users:create",
+            "users:list",
+            "users:show",
+            "users:update",
+            "users:delete",
+            "clients:list",
+            "clients:show",
+            "contracts:list",
+            "contracts:show",
+            "contracts:create",
+            "contracts:update",
+            "contracts:delete",
+            "events:list",
+            "events:show",
+            "events:update-support",
+        ],
+        "sales": [
+            "users:list",
+            "clients:create",
+            "clients:list",
+            "clients:show",
+            "clients:update",
+            "clients:delete",
+            "contracts:list",
+            "contracts:show",
+            "events:list",
+            "events:show",
+            "events:create",
+            "events:delete",
+        ],
+        "support": [
+            "clients:list",
+            "clients:show",
+            "contracts:list",
+            "contracts:show",
+            "events:list",
+            "events:show",
+            "events:update",
+        ],
+    }
+
+    if target in authorizations[user_role]:
+        return {"message": "Authorized"}, 200
+    return {"message": "Unauthorized"}, 403
